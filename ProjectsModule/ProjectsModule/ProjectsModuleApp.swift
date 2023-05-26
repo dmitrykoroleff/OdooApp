35dc299ddc841b10d621b20f4ea2afcfebab57d9
Dmitry Korolev <dmitrykorolev@Dmitrys-MacBook-Pro.local>
2023-05-26 11:54:12 +0300

    framework

---
+++ ProjectsModule/ProjectsModule/ProjectsModuleApp.swift
@@ -0,0 +1,22 @@
+//
+//  project_managementApp.swift
+//  project_management
+//
+//  Created by Dmitry Korolev on 30/3/2023.
+//
+
+import SwiftUI
+
+@main
+public struct ProjectsModuleApp: App {
+    public init() {}
+    public var body: some Scene {
+        WindowGroup {
+            InitView()
+                .onAppear{
+                    createTestData()
+                }
+               
+        }
+    }
+}
